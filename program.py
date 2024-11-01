22627e7c96b2b81a337a168e5f5507129146a23c
davronovask <abdulkhaevamuslima07@gmail.com>
2024-11-01 19:19:11 +0600

    убрала все коменты

--- program.py
+++ program.py
@@ -2,16 +2,14 @@ import random
 
 
 def generate_mines(size, num_mines):
-    # Создаем пустое поле
     board = [['0' for _ in range(size)] for _ in range(size)]
 
-    # Размещаем мины
     mines_placed = 0
     while mines_placed < num_mines:
         row = random.randint(0, size - 1)
         col = random.randint(0, size - 1)
 
-        # Проверяем, не стоит ли уже мина в этой клетке
+
         if board[row][col] != '*':
             board[row][col] = '*'
             mines_placed += 1
@@ -24,8 +22,8 @@ def display_board(board):
         print(' '.join(row))
 
 
-# Пример использования функции
-size = 5  # Размер поля
-num_mines = 5  # Количество мин
+
+size = 5
+num_mines = 5
 board = generate_mines(size, num_mines)
 display_board(board)
